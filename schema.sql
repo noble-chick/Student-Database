14f40d0892989bf1c12f5ee5003118878f10fdb1
noble-chick <tehreem2697@gmail.com>
2026-05-15 12:58:31 +0500

    edit comments schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,4 @@
--- ============================================================
 --  SCHEMA  —  run this file first
--- ============================================================
 
 CREATE TABLE instructors (
     instructor_id   SERIAL          PRIMARY KEY,
